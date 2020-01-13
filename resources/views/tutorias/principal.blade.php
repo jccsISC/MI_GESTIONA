558185fb3d0d337bad7506af8122a08f62515b2d
Kari <karina.pola444@gmail.com>
2020-01-12 18:08:17 -0600

    Borrado de scripts de vistas principal

--- resources/views/tutorias/principal.blade.php
+++ resources/views/tutorias/principal.blade.php
@@ -28,20 +28,4 @@
        
    </section>
 
-
-   <!--<script>
-
-        Drag.init (document.getElementById("windowAlumno"));
-
-        function abrir(){
-            document.getElementById("windowAlumno").style.display ="block";
-            document.getElementById("cardInfoP").style.background = "rgb(211, 211, 211)";
-        }
-
-        function cerrar(){
-            document.getElementById("windowAlumno").style.display = "none";
-            document.getElementById("cardInfoP").style.background = "white";
-        }
-
-    </script>-->
 @endsection
